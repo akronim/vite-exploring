c768b1f770c58dbde070d3e8ce5d3b2fc8f9d458
marinko <marinko.miskovic.hr@gmail.com>
2024-05-05 14:01:45 +0200

    use svgs

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 import './src/02.js';
 import './src/assets/05.css';
 import './src/assets/06.scss';
+import imgUrl from './src/assets/img/javascript.svg';
+import imgRaw from './src/assets/img/javascript.svg?raw';
+
+console.log(imgUrl);
+console.log(imgRaw);
--- src/02.js
+++ src/02.js
@@ -1,8 +1,10 @@
 import collect from "collect.js";
 import jsonData from '../src/assets/07.json';
+import jsonDataUrl from '../src/assets/07.json?url';
 
 console.log(
     collect([1, 3, 3, 7]).avg()
 );
 
 console.log(jsonData);
+console.log(jsonDataUrl);
