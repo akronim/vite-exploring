cf5ba6a2faa02dc9efec7696b941a6e4392695bd
marinko <marinko.miskovic.hr@gmail.com>
2024-05-06 21:11:31 +0200

    use aliases

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-import './src/02.js';
-import './src/assets/05.css';
-import './src/assets/06.scss';
-import imgUrl from './src/assets/img/javascript.svg';
-import imgRaw from './src/assets/img/javascript.svg?raw';
-import { template } from './src/12.jsx';
+import '@/02.js';
+import '@/assets/05.css';
+import '@/assets/06.scss';
+import imgUrl from '@/assets/img/javascript.svg';
+import imgRaw from '@/assets/img/javascript.svg?raw';
+import { template } from '@/12.jsx';
 
 console.log(imgUrl);
 console.log(imgRaw);
 
 // "glob" modules will be dynamically imported, unless: eager: true
-const modules = import.meta.glob('./src/10/*.js',
+const modules = import.meta.glob('@/10/*.js',
     {
         //eager: true
         //as: 'raw'
--- src/02.js
+++ src/02.js
@@ -1,6 +1,6 @@
 import collect from "collect.js";
-import jsonData from '../src/assets/07.json';
-import jsonDataUrl from '../src/assets/07.json?url';
+import jsonData from '@/assets/07.json';
+import jsonDataUrl from '@/assets/07.json?url';
 
 console.log(
     collect([1, 3, 3, 7]).avg()
