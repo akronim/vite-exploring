2cb1e19d5536b4037b229ef830d911307ec3886f
marinko <marinko.miskovic.hr@gmail.com>
2024-05-15 20:57:11 +0200

    use external dependecies

--- library-mode/lib/src/index.js
+++ library-mode/lib/src/index.js
@@ -1,3 +1,9 @@
-export function pluck(items, field) {
-    return items.map((item) => item[field]);
+import collect from "collect.js";
+
+export function pluckAndLog(items, field) {
+    const plucked = collect(items).pluck(field);
+
+    console.log(plucked);
+
+    return plucked;
 }
