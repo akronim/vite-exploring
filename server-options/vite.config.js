1b26719bba1aefc71294b14ea8970b1e7c6bf692
marinko <marinko.miskovic.hr@gmail.com>
2024-05-26 18:02:43 +0200

    server options

--- server-options/vite.config.js
+++ server-options/vite.config.js
@@ -1,29 +1,49 @@
-export default {
-    server: {
-        port: '3333',
-        strictPort: true,
-        headers: {
-            a: 'b'
+// export default {}
+
+import { defineConfig, loadEnv } from "vite";
+
+export default ({ command, mode, isSsrBuild }) => {
+    console.log(command, mode, isSsrBuild);
+
+    // load app-level env vars to node-level env vars
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    console.log(process.env.VITE_DBNAME);
+
+    const env = loadEnv(mode, process.cwd(), "VITE_");
+    console.log(env);
+
+    return defineConfig({
+        // base: command === 'build' ? '/prod/base/path/' : '/',
+        server: {
+            port: '3333',
+            strictPort: true,
+            headers: {
+                a: 'b'
+            },
+            open: 'index.html',
+            proxy: {
+                //'/products': 'https://dummyjson.com',
+                '/api': {
+                    target: 'https://dummyjson.com',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                }
+            }
         },
-        proxy: {
-            //'/products': 'https://dummyjson.com',
-            '/api': {
-                target: 'https://dummyjson.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
+        preview: {
+            port: '4444',
+            strictPort: true,
+            open: '/api/posts',
+            proxy: {
+                '/api': {
+                    target: 'https://dummyjson.com',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                }
             }
+        },
+        css: {
+            devSourcemap: true
         }
-    },
-    preview: {
-        port: '4444',
-        strictPort: true,
-        open: '/api/posts',
-        proxy: {
-            '/api': {
-                target: 'https://dummyjson.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            }
-        }
-    }
+    });
 }
