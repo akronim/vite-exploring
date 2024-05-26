5ce74c48cc86e2d9b0a73c3b0ab6863929d7e880
marinko <marinko.miskovic.hr@gmail.com>
2024-05-26 14:00:53 +0200

    static assets

--- README.md
+++ README.md
@@ -33,7 +33,6 @@ npm i -D sass
 ```
 
 ### public folder
-- files inside it can not be referenced from within source code files like main.js
 - files inside it are not processed by vite
 
 ### glob function
---
+++ static-assets/main.js
@@ -0,0 +1,7 @@
+import imgUrl from './src/assets/img/javascript.svg';
+import imgRaw from './src/assets/img/javascript.svg?raw';
+
+console.log(imgUrl);
+console.log(imgRaw);
+
+document.getElementById('image').src = imgUrl;
